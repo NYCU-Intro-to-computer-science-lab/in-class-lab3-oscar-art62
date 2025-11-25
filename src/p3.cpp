b933825ad7d912a4d94ad3d72760fa13717b76c3
oscar-art62 <u114705021.mg14@nycu.edu.tw>
2025-11-25 13:50:32 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,28 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n){
+    int s=0;
+    for(int i=1;i<n;i++){
+        if(n%i==0){
+            s++;
+        }
+    }
+    if(s==1) return true;
+    else return false;
+}
+
 int main() {
-    
+    int n;
+    cin>>n;
+    for(int i=2;i<=n/2;i++){
+        if(isPrime(i)){
+            if(isPrime(n-i)){
+                cout<<i<<" "<<n-i<<endl;
+                break;
+            }
+        }
+    }
     
     return 0;
 }
