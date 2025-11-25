42afc5aae57a6ca3d73d0dbc9ed433b9bcec00e9
oscar-art62 <u114705021.mg14@nycu.edu.tw>
2025-11-25 14:36:25 +0800

    Fix factorial and sum functions in p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,22 +1,19 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <iostream>
 using namespace std;
+
 long long fac(int n){
-    if(n<1)return 1;
-    return n*fac(n-1);
+    if(n == 0) return 1;   
+    return n * fac(n - 1);
 }
 
 long long s(int n){
-    if(n==0) return 0;
-    if(n==1) return 1;
-    return n+s(n-1);
+    if(n == 0) return 0;   
+    return n + s(n - 1);
 }
 
 int main() {
     int n;
-    cin>>n;
-    cout<<fac(n)<<endl<<s(n);
-    
+    cin >> n;
+    cout << fac(n) << endl << s(n);
     return 0;
 }
