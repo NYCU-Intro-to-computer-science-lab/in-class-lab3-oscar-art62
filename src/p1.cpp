9bed4b6a3610f9f2d1a5136e393c408d547b66d2
oscar-art62 <u114705021.mg14@nycu.edu.tw>
2025-11-25 14:39:32 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-long long fac(int n){
+long long factorial(int n){
     if(n == 0) return 1;   
     return n * fac(n - 1);
 }
 
-long long s(int n){
+long long sum(int n){
     if(n == 0) return 0;   
     return n + s(n - 1);
 }
@@ -14,6 +14,6 @@ long long s(int n){
 int main() {
     int n;
     cin >> n;
-    cout << fac(n) << endl << s(n);
+    cout << factorial(n) << endl << sum(n);
     return 0;
 }
