98bd64887e28a1d9450f8c0bf6a550a5adf4ee0e
oscar-art62 <u114705021.mg14@nycu.edu.tw>
2025-11-25 13:35:54 +0800

    Add factorial and summation functions

--- src/p1.cpp
+++ src/p1.cpp
@@ -2,9 +2,20 @@
 
 #include <iostream>
 using namespace std;
+long long fac(int n){
+    if(n<1)return 1;
+    return n*fac(n-1);
+}
+
+long long s(int n){
+    if(n==1) return 1;
+    return n+s(n-1);
+}
 
 int main() {
-    
+    int n;
+    cin>>n;
+    cout<<fac(n)<<endl<<s(n);
     
     return 0;
 }
